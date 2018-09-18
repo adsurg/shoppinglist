ca92a1ce1b71cedb307f857550e9eace170b2540
Adam Surgenor <adam@surgenor.me>
2018-09-18 07:53:42 +0100

    Connect it all to Dynamo

--- shoppingListService/src/getList.js
+++ shoppingListService/src/getList.js
@@ -1,19 +1,31 @@
 'use strict';
 
-module.exports.getList = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': 'http://my-shopping-list-demo.s3-website-eu-west-1.amazonaws.com',
-    },
-    body: JSON.stringify([{
-        name: "lemons",
-        quantity: 2
-      },{
-        name: "Bananas",
-        quantity: 8
-      }]),
-  };
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
 
-  callback(null, response);
-};
+const dynamoDb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+module.exports.getList = (event, context, callback) => 
+  dynamoDb.scan({
+    TableName: "ShoppingListEntry",
+    Select:"ALL_ATTRIBUTES"
+  }, 
+  (err, data) => {
+    if(err){
+      callback(err);
+      return;
+    }
+
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://my-shopping-list-demo.s3-website-eu-west-1.amazonaws.com',
+      },
+      body: JSON.stringify(
+        data.Items.map(
+          item => ({
+              name: item.product.S,
+              quantity: Number(item.quantity.N)
+            })))
+    });
+  });
--- shoppingListService/src/putItem.js
+++ shoppingListService/src/putItem.js
@@ -1,17 +1,30 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-1'});
+
+const dynamoDb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
 module.exports.putItem = (event, context, callback) => {
   const listItem = JSON.parse(event.body)
 
-  console.log(JSON.stringify(listItem))
+  dynamoDb.putItem({
+    TableName: 'ShoppingListEntry',
+    Item: {
+      'product' : {S: listItem.name},
+      'quantity' : {N: `${listItem.quantity}`},
+    }
+  }, (err, data) => {
+    if(err){
+      callback(err)
+      return;
+    }
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': 'http://my-shopping-list-demo.s3-website-eu-west-1.amazonaws.com',
-    },
-    body: ""
-  };
-
-  callback(null, response);
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://my-shopping-list-demo.s3-website-eu-west-1.amazonaws.com',
+      },
+      body: ""
+    })})
 };
